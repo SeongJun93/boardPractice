269f283aaeb6d5e8a6c7e9fd766658ce4f3cd144
DESKTOP-2RI99DB\jo <jsj930623@daonlink.com>
2022-01-12 13:04:09 +0900

    asd

--- news/src/main/java/com/company/board/domain/Pagination.java
+++ news/src/main/java/com/company/board/domain/Pagination.java
@@ -132,6 +132,7 @@ public class Pagination {
 	}
 
 	// 첫번째 인자 page 는 현재 페이지 정보, 두번째 인자 range 는 현재 페이지 범위 정보, 세번째 인자 listCnt는 게시물의 총 개수
+	// 페이징 할시 첫번째로 세팅해야할 것 전체게시물 수 현재 보여줄 페이지 번호,페이지 범위
 	public void pageInfo(int page, int range, int listCnt) {
 
 		this.page = page;
@@ -141,21 +142,33 @@ public class Pagination {
 		this.listCnt = listCnt;
 
 		// 전체 페이지수
+		// 전체 페이지 갯수 = 총 게시글 갯수 / 보여줄 게시글 갯수
 		this.pageCnt = (int) Math.ceil((double) listCnt / listSize);
-
+		
 		// 시작 페이지
+		// 각 페이지 범위 시작 번호 = (각 페이지 범위 시작 번호  - 1) * 초기값 페이징 갯수+1
+		// 1/11/21/31/41 ......
 		this.startPage = (range - 1) * rangeSize + 1;
-
+		
 		// 끝 페이지
+		// 각 페이지 범위 끝 번호 = 각 페이지 범위 시작 번호 * 초기값 페이징 갯수
+		// 10/20/30/40 ......
 		this.endPage = range * rangeSize;
 
 		// 게시판 시작번호
+		// 게시판 시작 번호 = (현재목록의 페이지 번호 - 1) * 개시글 총 갯수
+		// 게시판은 0번 부터 시작 뒤에서
 		this.startListNum = (page - 1) * listSize;
 
 		// 이전 버튼 상태
+		// 삼항연산자 
+		// 이전 = 현재페이지 범위 == 1 ? 트루     :  거짓 
+		// 현제 페이지가 1일경우 1이 트루이면 false 거짓이면 이전버튼 반환
 		this.prev = range == 1 ? false : true;
 
 		// 다음 버튼 상태
+		// 삼항연산자
+		//    다음=각페이지 범위 끝 번호>페이지갯수 ?  참    :  거짓
 		this.next = endPage > pageCnt ? false : true;
 		if (this.endPage > this.pageCnt) {
 			this.endPage = this.pageCnt;
